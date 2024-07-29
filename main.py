d16b22cd42044fb3414b95680be560b66d963355
suvirghai <ghai.s@northeastern.edu>
2024-07-29 15:14:28 -0400

    i tried

--- main.py
+++ main.py
@@ -126,9 +126,9 @@ def main():
 
     col1, col2, col3 = st.columns([1, 2, 1])
     col1.header("Get to know me")
-    col2.image("memoji.png", width=200)
-    with open("resume.pdf", "rb") as file:
-        col3.download_button(label="Download my Resume", data=file, file_name="resume.pdf", mime="application/pdf")
+    #col2.image("memoji.png", width=200)
+   # with open("resume.pdf", "rb") as file:
+     #   col3.download_button(label="Download my Resume", data=file, file_name="resume.pdf", mime="application/pdf")
 
     st.write("Hi, I’m Suvir Ghai. Feel free to ask me any questions you have!")
     message = st.text_area("Your question here...")
